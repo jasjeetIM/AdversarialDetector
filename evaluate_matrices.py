a98fbb6683082b24a6e30aa20a92911a38aa1ec2
jasjeet <jasjeet_dhaliwal@symantec.com>
2018-02-17 16:36:38 +0000

    minor

--- evaluate_matrices.py
+++ evaluate_matrices.py
@@ -24,11 +24,11 @@ def get_similarity(option=0, device="0", seed = 12, model_name="CNN-MNIST-keras"
   model.load_model('./trained_models/' + model_name + '-model.json', './trained_models/' + model_name + 'weights')
   
   if option == 0:
-    sim_matrix, test_gradients, train_gradients = model.evaluate_similarity_matrix(use_reg_label=True,sim_save_path='similarity_matrices/reg_sim_0213.dat', train_grad_save_path='influence_matrices/train_grad_all.dat.npy',test_grad_save_path='similarity_matrices/reg_test_grad_0213.dat' )
+    sim_matrix, test_gradients, train_gradients = model.evaluate_similarity_matrix(use_reg_label=True,sim_save_path='similarity_matrices/reg_sim_10k.dat.npy', train_grad_save_path='influence_matrices/train_grad_10k.dat.npy',use_test_grad_matrix=True, test_grad_matrix_path='similarity_matrices/reg_test_grad.dat.npy' )
   elif option == 1:
-    sim_matrix, test_gradients, train_gradients = model.evaluate_similarity_matrix(use_adv_sample=True, use_adv_label=True, sim_save_path='similarity_matrices/adv_sim_0213.dat', use_train_grad_matrix=True, train_grad_matrix_path='influence_matrices/train_grad_all.dat.npy',test_grad_save_path='similarity_matrices/adv_test_grad_0213.dat' )
+    sim_matrix, test_gradients, train_gradients = model.evaluate_similarity_matrix(use_adv_sample=True, use_adv_label=True, sim_save_path='similarity_matrices/adv_sim_10k.dat.npy', use_train_grad_matrix=True, train_grad_matrix_path='influence_matrices/train_grad_10k.dat.npy',use_test_grad_matrix=True,test_grad_matrix_path='similarity_matrices/adv_test_grad.dat.npy' )
   elif option == 2:
-    sim_matrix, test_gradients, train_gradients = model.evaluate_similarity_matrix(use_rand_sample=True, use_rand_label=True,sim_save_path='similarity_matrices/rand_sim_0213.dat', use_train_grad_matrix=True, train_grad_matrix_path='influence_matrices/train_grad_all.dat.npy',test_grad_save_path='similarity_matrices/rand_test_grad_0213.dat' )
+    sim_matrix, test_gradients, train_gradients = model.evaluate_similarity_matrix(use_rand_sample=True, use_rand_label=True,sim_save_path='similarity_matrices/rand_sim_10k.dat.npy', use_train_grad_matrix=True, train_grad_matrix_path='influence_matrices/train_grad_10k.dat.npy',use_test_grad_matrix=True, test_grad_matrix_path='similarity_matrices/rand_test_grad.dat.npy' )
 
 def get_influence(option=0, device="0", seed = 12, model_name="CNN-MNIST-keras"):
   """
@@ -43,11 +43,11 @@ def get_influence(option=0, device="0", seed = 12, model_name="CNN-MNIST-keras")
   model.load_model('./trained_models/' + model_name + '-model.json', './trained_models/' + model_name + 'weights')
   
   if option == 0:
-    inf_matrix, hvp_matrix, train_gradients = model.evaluate_influence_matrix(use_reg_label=True,inf_save_path='influence_matrices/reg_inf_0213.dat', use_grad_matrix=True, grad_matrix_path='influence_matrices/train_grad_all.dat.npy', hvp_save_path='influence_matrices/reg_hvp_0213.dat')
+    inf_matrix, hvp_matrix, train_gradients = model.evaluate_influence_matrix(use_reg_label=True,inf_save_path='influence_matrices/reg_inf_10k.dat.npy', use_grad_matrix=True, grad_matrix_path='influence_matrices/train_grad_10k.dat.npy', use_hvp_matrix=True,hvp_matrix_path='influence_matrices/reg_hvp.dat.npy')
   elif option == 1:
-    inf_matrix, hvp_matrix, train_gradients = model.evaluate_influence_matrix(use_adv_sample=True,use_adv_label=True,inf_save_path='influence_matrices/adv_inf_0213.dat', use_grad_matrix=True, grad_matrix_path='influence_matrices/train_grad_all.dat.npy', hvp_save_path='influence_matrices/adv_hvp_0213.dat')
+    inf_matrix, hvp_matrix, train_gradients = model.evaluate_influence_matrix(use_adv_sample=True,use_adv_label=True,inf_save_path='influence_matrices/adv_inf_10k.dat.npy', use_grad_matrix=True, grad_matrix_path='influence_matrices/train_grad_10k.dat.npy', use_hvp_matrix=True, hvp_matrix_path='influence_matrices/adv_hvp.dat.npy')
   elif option == 2:
-    inf_matrix, hvp_matrix, train_gradients = model.evaluate_influence_matrix(use_rand_sample=True,use_rand_label=True, inf_save_path='influence_matrices/rand_inf_0213.dat', use_grad_matrix=True, grad_matrix_path='influence_matrices/train_grad_all.dat.npy',hvp_save_path='influence_matrices/rand_hvp_0213.dat')
+    inf_matrix, hvp_matrix, train_gradients = model.evaluate_influence_matrix(use_rand_sample=True,use_rand_label=True, inf_save_path='influence_matrices/rand_inf_10k.dat.npy', use_grad_matrix=True, grad_matrix_path='influence_matrices/train_grad_10k.dat.npy',use_hvp_matrix=True,hvp_matrix_path='influence_matrices/rand_hvp.dat.npy')
 
 
 if __name__ == '__main__':
