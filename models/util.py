74172fc08339acc3780e39affec97d3cbc1d50df
jasjeet <jasjeet_dhaliwal@symantec.com>
2018-04-09 16:05:27 +0000

    first set of exp done

---
+++ models/util.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+def avg_l2_dist(orig, adv):
+    """Get the mean l2 distortion between two orig and adv images"""
+    l2_dist = 0.0
+    for i in range(orig.shape[0]):
+        l2_dist+= np.linalg.norm(orig[i] - adv[i])
+    return l2_dist/orig.shape[0]
+
+
+def visualize(image):
+   """Visualize gray scale or color image"""
+    plt.figure(figsize=(1, 1))
+    if image.shape[-1] == 1:
+        # image is in black and white
+        image = image[:, :, 0]
+        plt.imshow(image, cmap='Greys')
+    else:
+        # image is in color
+        plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+
+#Normalize rows of a given matrix
+def normalize(matrix):
+   """Normalize each row vector in a matrix"""
+    matrix_nm = np.zeros_like(matrix)
+    for i in range(matrix.shape[0]):
+        matrix_nm[i] = matrix[i]/np.linalg.norm(matrix[i]) 
+    return matrix_nm
+
+
